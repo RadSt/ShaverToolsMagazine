13181fd506935b92349dd24b448c64eb714b3f14
RadSt <desan1986@gmail.com>
2017-01-03 18:44:03 +0300

    fix test path for travis.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 dotnet restore
-dotnet test ShaverToolsShop/ShaverToolsShop.Tests
+dotnet test ShaverToolsShop/ShaverToolsShop.Test
 dotnet build -c Release ShaverToolsShop/src/ShaverToolsShop
