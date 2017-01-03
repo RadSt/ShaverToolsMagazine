ebae27f6320f061b1fca376ca7a3c8484e3ecdda
RadSt <desan1986@gmail.com>
2017-01-03 21:21:44 +0300

    Added test ShouldAddSubscription_WhenWeAddSubscription.

--- ShaverToolsShop/ShaverToolsShop.Test/SubscriptionServiceTest.cs
+++ ShaverToolsShop/ShaverToolsShop.Test/SubscriptionServiceTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
 using ShaverToolsShop.Conventions.Repositories;
@@ -16,13 +14,17 @@ namespace ShaverToolsShop.Test
     public class CalculateSubscriptionSum
     {
         private Mock<ISubscriptionReadRepository> _subscriptionReadRepository;
+        private Mock<ISubscriptionRepository> _subscriptionRepository;
         private ISubscriptionService _subscriptionService;
         private List<Subscription> _subscriptions;
+        private Subscription _subscription;
+
 
         [SetUp]
         public void SetUp()
         {
             _subscriptionReadRepository = new Mock<ISubscriptionReadRepository>();
+            _subscriptionRepository = new Mock<ISubscriptionRepository>();
             _subscriptionService = new SubscriptionService(_subscriptionReadRepository.Object);
             _subscriptions = new List<Subscription>
             {
@@ -45,6 +47,26 @@ namespace ShaverToolsShop.Test
                     }
                 }
             };
+            _subscription =
+                new Subscription
+                {
+                    StartDate = DateTime.Parse("01/01/2017"),
+                    EndDate = DateTime.Parse("03/01/2017"),
+                    Products = new List<Product>
+                    {
+                        new Product
+                        {
+                            Name = "Бритвенный станок",
+                            Price = 1
+                        },
+                        new Product
+                        {
+                            Name = "Средство после бритья",
+                            Price = 10
+                        }
+                    }
+                };
+
         }
         [Test]
         public async Task ShouldReturnSubscriptions_WhenWeAskAllSubscriptions()
@@ -57,6 +79,23 @@ namespace ShaverToolsShop.Test
 
             //Assert
             Assert.IsNotNull(results);
+        }
+        [Test]
+        public void ShouldAddSubscription_WhenWeAddSubscription()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            _subscriptionRepository.Setup(m => m.Add(_subscription)).ReturnsAsync((Subscription e) =>
+            {
+                e.Id = id;
+                return e;
+            });
+
+            //Act
+            _subscriptionService.Create(_subscriptions);
+
+            //Assert
+            Assert.AreEqual(id, _subscription.Id);
         }
     }
 }
